46acd568e4b15464a96c6a6f6ac9957a830a3ff6
cyxn <nick.sukhov@eze.tech>
2025-09-21 13:35:49 +0200

    WIP

--- app/(flavours)/index.tsx
+++ app/(flavours)/index.tsx
@@ -1,9 +1,11 @@
-import { Button, ContextMenu, HStack, Host, Image, Spacer, Text } from '@expo/ui/swift-ui';
-import { Link, Stack } from 'expo-router';
-import { FlatList, useColorScheme } from 'react-native';
-
 import { FlavourList } from '@/model';
-import { frame } from '@expo/ui/swift-ui/modifiers';
+import { Button, ContextMenu, Group, Host, HStack, Image, Text, VStack } from '@expo/ui/swift-ui';
+import { background, border, clipShape, frame, padding } from '@expo/ui/swift-ui/modifiers';
+import { FlashList } from '@shopify/flash-list';
+import { Link, Stack, useRouter } from 'expo-router';
+import { Dimensions, useColorScheme, View } from 'react-native';
+
+const screenWidth = Dimensions.get('window').width;
 
 export default function Index() {
   const colorScheme = useColorScheme();
@@ -12,74 +14,108 @@ export default function Index() {
     <>
       <Stack.Screen
         options={{
-          title: 'Flavour List',
-          headerLargeTitle: true,
-          headerSearchBarOptions: {
-            hideWhenScrolling: true,
-          },
-          headerRight: () => {
-            return (
-              <Host matchContents>
-                <ContextMenu>
-                  <ContextMenu.Items>
-                    <Button onPress={() => {}}>Show Favourites Only</Button>
-                    <Button onPress={() => {}}>Hide Tasted</Button>
-                    <Button onPress={() => {}}>Show Current Only</Button>
-                    <Button onPress={() => {}}>Show Vegan Only</Button>
-                    <Button onPress={() => {}}>Show Dairy Free Only</Button>
-                    <Button onPress={() => {}}>Show Gluten Free Only</Button>
-                    <Button onPress={() => {}}>Show Nut Free Only</Button>
-                    <Button onPress={() => {}}>Show Alcohol Free Only</Button>
-                  </ContextMenu.Items>
-                  <ContextMenu.Trigger>
-                    {/* iOS 26 header buttons have height and width of 36, so we add static width 
-                    and height to keep the entire area tappable */}
-                    <HStack modifiers={[frame({ width: 36, height: 36 })]}>
-                      <Image systemName="line.3.horizontal.decrease.circle" size={28} />
-                    </HStack>
-                  </ContextMenu.Trigger>
-                </ContextMenu>
-              </Host>
-            );
-          },
+          title: 'list',
+          headerShown: true,
+          headerBackButtonDisplayMode: 'minimal',
+          headerTransparent: true,
         }}
       />
-      <Host style={{ flex: 1 }} colorScheme={colorScheme}>
-        <FlatList
-          data={FlavourList}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item, index }) => (
-            <Link href={`/flavours/${item.id}`} asChild>
-              <Link.Trigger>
-                <FlavourItem item={item} index={index} />
-              </Link.Trigger>
-              <Link.Preview>
-                <FlavourItem item={item} index={index} />
-              </Link.Preview>
-              <Link.Menu>
-                <Link.MenuAction icon="eye" title="View" onPress={() => {}} />
-                <Link.MenuAction icon="pencil" title="Edit" onPress={() => {}} />
-                <Link.MenuAction icon="trash" destructive title="Delete" onPress={() => {}} />
-              </Link.Menu>
-            </Link>
-          )}
-        />
-      </Host>
+      <FlashList
+        data={FlavourList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <Link href={`/flavours/${item.id}`} asChild>
+            <Link.Trigger>
+              <FlavourItem item={item} index={index} />
+            </Link.Trigger>
+            <Link.Preview
+              style={{
+                height: 100,
+                width: screenWidth - 12,
+                backgroundColor: '#fff',
+              }}>
+              <FlavourItem item={item} index={index} />
+            </Link.Preview>
+            <Link.Menu>
+              <Link.MenuAction icon="eye" title="View" onPress={() => {}} />
+              <Link.MenuAction icon="pencil" title="Edit" onPress={() => {}} />
+              <Link.MenuAction icon="trash" destructive title="Delete" onPress={() => {}} />
+            </Link.Menu>
+          </Link>
+        )}
+      />
     </>
   );
 }
 
 function FlavourItem({ item, index }: { item: any; index: number }) {
+  const cardWidth = screenWidth - 12;
+
+  const cardModifiers = [
+    frame({
+      width: cardWidth,
+      height: 100,
+    }),
+    background('red'),
+    border({
+      color: 'black',
+      width: 1,
+    }),
+    clipShape('roundedRectangle', 32),
+  ];
+
+  const router = useRouter();
   return (
-    <Host matchContents modifiers={[frame({ width: 100, height: 64 })]}>
-      <Button>
-        <HStack spacing={8}>
-          <Text size={14} color="secondary">{`#${index + 1}:`}</Text>
-          <Text size={14} color="primary">{`${item.name}`}</Text>
-          <Spacer />
-          <Image systemName="chevron.right" size={14} color="secondary" />
-        </HStack>
-      </Button>
-    </Host>
+    <View style={{ height: 100, width: cardWidth, marginBottom: 6, backgroundColor: 'blue' }}>
+      <Host
+        style={{ height: 100, width: cardWidth, margin: 0 }}
+        modifiers={[
+          frame({
+            width: cardWidth,
+            height: 100,
+          }),
+        ]}>
+        <Group
+          onPress={() => {
+            router.push(`/flavours/${item.id}`);
+          }}
+          modifiers={cardModifiers}>
+          <HStack
+            alignment="center"
+            modifiers={[
+              padding({
+                leading: 16,
+              }),
+            ]}>
+            <VStack alignment="leading">
+              <Text size={14} color="primary">
+                {item.name}
+              </Text>
+            </VStack>
+            <ContextMenu activationMethod="singlePress">
+              <ContextMenu.Items>
+                <Button key={'edit'} systemImage={'pencil'} onPress={() => {}}>
+                  {'edit'}
+                </Button>
+              </ContextMenu.Items>
+              <ContextMenu.Trigger>
+                <Image
+                  systemName="ellipsis"
+                  color={'black'}
+                  size={50}
+                  modifiers={[
+                    frame({
+                      width: 50,
+                      height: 50,
+                      alignment: 'center',
+                    }),
+                  ]}
+                />
+              </ContextMenu.Trigger>
+            </ContextMenu>
+          </HStack>
+        </Group>
+      </Host>
+    </View>
   );
 }
